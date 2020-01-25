9651169ee254233fea2f8aed3216a125fb15bc1a
shuhei <shukuri.7336.8@gmail.com>
2020-01-25 16:39:33 +0900

    collectstatic

--- taskManager/forms.py
+++ taskManager/forms.py
@@ -1,4 +1,5 @@
-from django.forms import Form, CharField, DateTimeField, ModelForm
+from django.forms import Form, CharField, ModelForm
+from django import forms
 from django.contrib.auth.models import User
 from .models import Task
 # from django.utils import timezone
@@ -12,11 +13,18 @@ class TaskForm(ModelForm):
     class Meta:
         model = Task
         fields = ['name','deadline','when']
+        widgets = {
+            'deadline': forms.SelectDateWidget,
+            'when': forms.SelectDateWidget
+        }
 
 class DoneEditForm(ModelForm):
     class Meta:
         model = Task
         fields = ['done_date']
+        widgets = {
+            'done_date': forms.SelectDateWidget
+        }
 
 # class EditForm(Form, task):
 #     name = CharField('タスク名', max_length=256, default=task.name)
--- taskManager/settings/common.py
+++ taskManager/settings/common.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -52,7 +52,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, "templates")],
-        'APP_DIRS': True,
+        'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
