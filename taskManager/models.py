23f96f0c859c6eb65c373af905e44027ac0f0f85
shuhei <shukuri.7336.8@gmail.com>
2020-01-25 19:52:42 +0900

    細かい変更

--- taskManager/models.py
+++ taskManager/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-import datetime
+from django.utils.timezone import now
 from django.contrib.auth.models import User
 
 class Task(models.Model):
     name = models.CharField('タスク名', max_length=256, blank=True)
     # id = models.AutoField(primary_key=True)
-    deadline = models.DateField('期限', default=datetime.date.today(), blank=True, null=True)
-    when = models.DateField('実行予定日', default=datetime.date.today(), blank=True)
+    deadline = models.DateField('期限', default=now, blank=True, null=True)
+    when = models.DateField('実行予定日', default=now, blank=True)
     done_or_not = models.BooleanField(default=False)
-    done_date = models.DateField('完了した日', default=datetime.date.today(), blank=True, null=True)
+    done_date = models.DateField('完了した日', default=now, blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
