b19e3eda7ac51fc2e16eb5adf939ee03422401b7
shuhei <shukuri.7336.8@gmail.com>
2020-02-02 03:20:41 +0900

    カレンダー入力機能

--- taskManager/forms.py
+++ taskManager/forms.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 from .models import Task
 # from django.utils import timezone
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 class UserForm(ModelForm):
     class Meta:
         model = User
@@ -15,8 +18,8 @@ class TaskForm(ModelForm):
         fields = ['name','when','deadline','important','urgent']
         forms.CheckboxInput(attrs={'class': 'check'})
         widgets = {
-            # 'deadline': forms.SelectDateWidget,
-            # 'when': forms.SelectDateWidget,
+            'deadline': DateInput(),
+            'when': DateInput(),
             # 'important': forms.CheckboxInput(),
             # 'urgent': forms.CheckboxInput()
         }
@@ -25,9 +28,9 @@ class DoneEditForm(ModelForm):
     class Meta:
         model = Task
         fields = ['done_date']
-        # widgets = {
-        #     'done_date': forms.SelectDateWidget
-        # }
+        widgets = {
+            'done_date': DateInput()
+        }
 
 # class EditForm(Form, task):
 #     name = CharField('タスク名', max_length=256, default=task.name)
