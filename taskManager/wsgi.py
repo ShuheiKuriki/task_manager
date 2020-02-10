cd585a7eb162e1020803f11dbfdc2b0f3b84db9d
shuhei <shukuri.7336.8@gmail.com>
2020-02-10 17:03:27 +0900

    修正

--- taskManager/wsgi.py
+++ taskManager/wsgi.py
@@ -11,14 +11,15 @@ import os
 import time
 import schedule
 import threading
+import requests
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskManager.settings.prod')
 
 application = get_wsgi_application()
 
-def notify(msg):
-    requests.get("http://tasks-day-scheduler.herokuapp.com/notify")
+def notify():
+    requests.get("https://tasks-day-scheduler.herokuapp.com/notify")
 
 def daily():
     schedule.every().day.at("23:30").do(notify)
