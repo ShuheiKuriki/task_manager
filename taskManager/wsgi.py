b0edad807d5983d88963fcc75494c834b128da7c
shuhei <shukuri.7336.8@gmail.com>
2020-02-11 15:39:37 +0900

    修正

--- taskManager/wsgi.py
+++ taskManager/wsgi.py
@@ -19,8 +19,8 @@ logger = logging.getLogger(__name__)
 # logger.setLevel(10)
 pid = os.getpid()
 thread_name = threading.current_thread().getName()
-logger.error("pid:{}".format(pid))
-logger.error("thread_name:{}".format(thread_name))
+logger.info("pid:{}".format(pid))
+logger.info("thread_name:{}".format(thread_name))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskManager.settings.prod')
 
@@ -29,49 +29,19 @@ application = get_wsgi_application()
 def notify(when):
     requests.get("https://tasks-day-scheduler.herokuapp.com/notify/"+when)
 
-def night():
-    schedule.every().day.at("15:09").do(notify, "night")
-    logger.error("night")
-    while True:
-        schedule.run_pending()
-        logger.error("night_pending")
-        time.sleep(60)
-        logger.error("night_sleep_done")
-
-def morning():
-    schedule.every().day.at("15:10").do(notify, "morning")
-    logger.error("morning")
-    while True:
-        schedule.run_pending()
-        logger.error("morning_pending")
-        time.sleep(60)
-        logger.error("morning_sleep_done")
-
-def afternoon():
-    schedule.every().day.at("15:11").do(notify, "afternoon")
-    logger.error("afternoon")
-    while True:
-        schedule.run_pending()
-        logger.error("afternoon_pending")
-        time.sleep(60)
-        logger.error("afternoon_sleep_done")
-
-def minutely():
+def daily():
+    schedule.every().day.at("15:42").do(notify, "morning")
+    schedule.every().day.at("15:44").do(notify, "afternoon")
+    schedule.every().day.at("15:46").do(notify, "night")
     # schedule.every(2).minutes.do(notify, "a")
-    notify("a")
-    logger.error("notify")
-    # while True:
-    #     schedule.run_pending()
-    #     logger.error("I'm going to bed")
-    #     time.sleep(60)
-    #     logger.error("I'm awake")
+    while True:
+        schedule.run_pending()
+        logger.info("pending")
+        time.sleep(60)
+        logger.info("sleep_done")
+
 def test():
     logger.error("test")
 
-t = threading.Thread(target=night)
-s = threading.Thread(target=morning)
-u = threading.Thread(target=afternoon)
-logger.error("start")
-s.start()
+t = threading.Thread(target=daily)
 t.start()
-u.start()
