a624a5c2f82e370f9021bf04d2ec4411628cc9d1
shuhei <shukuri.7336.8@gmail.com>
2020-02-12 09:07:18 +0900

    改良

--- taskManager/wsgi.py
+++ taskManager/wsgi.py
@@ -19,8 +19,8 @@ logger = logging.getLogger(__name__)
 # logger.setLevel(10)
 pid = os.getpid()
 thread_name = threading.current_thread().getName()
-logger.error("pid:{}".format(pid))
-logger.error("thread_name:{}".format(thread_name))
+print("pid:{}".format(pid))
+print("thread_name:{}".format(thread_name))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskManager.settings.prod')
 
@@ -30,19 +30,21 @@ def notify(when):
     requests.get("https://tasks-day-scheduler.herokuapp.com/notify/"+when)
 
 def daily():
-    schedule.every().day.at("09:00").do(notify, "morning")
-    schedule.every().day.at("16:00").do(notify, "afternoon")
+    schedule.every().day.at("08:00").do(notify, "morning")
+    schedule.every().day.at("15:00").do(notify, "afternoon")
     schedule.every().day.at("22:30").do(notify, "night")
     # schedule.every(2).minutes.do(notify, "a")
     while True:
-        schedule.run_pending()
-        logger.error("pending")
-        time.sleep(120)
-        requests.get("https://tasks-day-scheduler.herokuapp.com/")
-        logger.error("sleep_done")
+        for i in range(10):
+            schedule.run_pending()
+            print("pending")
+            time.sleep(60)
+            if i == 0:
+                requests.get("https://tasks-day-scheduler.herokuapp.com/")
+            print("sleep_done")
 
 def test():
-    logger.error("test")
+    print("test")
 
 t = threading.Thread(target=daily)
 t.start()
