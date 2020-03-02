c52c7ccb3c4ae061e6615fe8f1e476996acea8c5
shuhei <shukuri.7336.8@gmail.com>
2020-03-03 04:58:10 +0900

    ログイン新規登録フォーム修正

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,6 +4,6 @@ class LoginForm(AuthenticationForm):
     #ログオンフォームの定義
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for fields in self.fields.values():
-            fields.widget.attrs['class'] = 'form-control'
-            fields.widget.attrs['placeholder']= fields.label
+        # for fields in self.fields.values():
+        #     fields.widget.attrs['class'] = 'form-control'
+        #     fields.widget.attrs['placeholder']= fields.label
