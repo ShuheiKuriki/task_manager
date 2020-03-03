3cd4b84d6ffc88d5b74fd80746cd6418d844df92
shuhei <shukuri.7336.8@gmail.com>
2020-03-04 02:06:33 +0900

    一覧テンプレート共通化

--- accounts/views.py
+++ accounts/views.py
@@ -17,15 +17,12 @@ class Login(LoginView):
     form_class = LoginForm
     template_name = 'Form/login.html'
 
-
 class Logout(LoginRequiredMixin, LogoutView):
     #ログアウトページ
     template_name = 'Form/login.html'
 
-
 def profile(request):
     return redirect('/accounts/'+str(request.user.id))
-
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
@@ -50,21 +47,23 @@ def index(request,pk):
         tasks=tasks.filter(when__gt=datetime.date.today()+datetime.timedelta(days=1)).order_by('when')
     )
     infos = [today, tom, other]
-    return render(request, 'Menu/list.html', {'infos':infos})
+    return render(request, 'Menu/all_list.html', {'infos':infos})
 
 def today(request,pk):
     if request.user.pk != pk:
         return redirect('login')
-    tasks = Task.objects.all().filter(user=request.user, done_or_not=False, when__lte=datetime.date.today()).order_by('order')
-    num = len(tasks)
-    return render(request, 'Menu/today.html', {'tasks':tasks,'num':num})
+    tasks = Task.objects.all().filter(user=request.user, done_or_not=False)
+    info = Taskinfo(tasks=tasks.filter(when__lte=datetime.date.today()).order_by('order'))
+    infos = [info]
+    return render(request, 'Menu/today.html', {'infos':infos})
 
 def tomorrow(request,pk):
     if request.user.pk != pk:
         return redirect('login')
-    tasks = Task.objects.all().filter(user=request.user, done_or_not=False, when=datetime.date.today()+datetime.timedelta(days=1)).order_by('order')
-    num = len(tasks)
-    return render(request, 'Menu/tomorrow.html', {'tasks':tasks,'num':num})
+    tasks = Task.objects.all().filter(user=request.user, done_or_not=False)
+    info = Taskinfo(tasks=tasks.filter(when=datetime.date.today()+datetime.timedelta(days=1)).order_by('order'))
+    infos = [info]
+    return render(request, 'Menu/tomorrow.html', {'infos':infos})
 
 def done_list(request,pk):
     if request.user.pk != pk:
