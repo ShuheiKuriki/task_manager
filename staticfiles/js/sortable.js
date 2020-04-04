3c27b63c357ad30de6b6b8abb38a036f29b520a0
shuhei <shukuri.7336.8@gmail.com>
2020-04-04 11:04:07 +0900

    UI改善

--- staticfiles/js/sortable.js
+++ staticfiles/js/sortable.js
@@ -179,7 +179,37 @@ $(document).ready(function() {
           });
         }
       });
+      for (  var i = 1;  i < 6;  i++  ) {
+        $('.sortable'+String(i)).bind('sortstop',function(){
+          // 番号を設定している要素に対しループ処理
+            $(this).find('[name="num_data"]').each(function(idx){
+          // タグ内に通し番号を設定（idxは0始まりなので+1する）
+                $(this).html(idx+1);
+            });
+          });
+      };
+      for (  var i = 6;  i < 11;  i++  ) {
+        $('.sortable'+String(i)).bind('sortstop',function(){
+          // 番号を設定している要素に対しループ処理
+            $(this).find('[name="num_data2"]').each(function(idx){
+          // タグ内に通し番号を設定（idxは0始まりなので+1する）
+                $(this).html(idx+1);
+            });
+          });
+      };
+      for (  var i = 11;  i < 16;  i++  ) {
+        $('.sortable'+String(i)).bind('sortstop',function(){
+          // 番号を設定している要素に対しループ処理
+            $(this).find('[name="num_data3"]').each(function(idx){
+          // タグ内に通し番号を設定（idxは0始まりなので+1する）
+                $(this).html(idx+1);
+            });
+          });
+      };
 });
+  for (  var i = 1;  i < 16;  i++  ) {
+    $('.sortable'+String(i)).disableSelection();
+  }
 
 function done_alert() {
     alert('タスクを完了しました。\nおめでとうございます！！');
