9a5ef7ae1dd54fb07f706ad5463a785c410744bb
shuhei <shukuri.7336.8@gmail.com>
2020-04-06 20:51:53 +0900

    collectstatic

--- staticfiles/js/sortable.js
+++ staticfiles/js/sortable.js
@@ -1,20 +1,366 @@
 $(document).ready(function() {
-    for (  var i = 16;  i < 31;  i++  ) {
-      $('.sortable'+String(i)).sortable({
+      $('#sortable1').sortable({
         cursor: 'move',
         opacity: 0.5,
         update: function(event, ui) {
-          var serial = $('.sortable'+String(i)).sortable('serialize');
+          var serial = $('#sortable1').sortable('serialize');
+          $.ajax({
+            url: "/task/sort",
+            type: "post",
+            data: serial
+          });
+        }
+      });
+      $('#sortable2').sortable({
+        cursor: 'move',
+        opacity: 0.5,
+        update: function(event, ui) {
+          var serial = $('#sortable2').sortable('serialize');
+          $.ajax({
+            url: "/task/sort",
+            type: "post",
+            data: serial
+          });
+        }
+      });
+      $('#sortable3').sortable({
+        cursor: 'move',
+        opacity: 0.5,
+        update: function(event, ui) {
+          var serial = $('#sortable3').sortable('serialize');
+          $.ajax({
+            url: "/task/sort",
+            type: "post",
+            data: serial
+          });
+        }
+      });
+      $('#sortable4').sortable({
+        cursor: 'move',
+        opacity: 0.5,
+        update: function(event, ui) {
+          var serial = $('#sortable4').sortable('serialize');
+          $.ajax({
+            url: "/task/sort",
+            type: "post",
+            data: serial
+          });
+        }
+      });
+      $('#sortable5').sortable({
+        cursor: 'move',
+        opacity: 0.5,
+        update: function(event, ui) {
+          var serial = $('#sortable5').sortable('serialize');
+          $.ajax({
+            url: "/task/sort",
+            type: "post",
+            data: serial
+          });
+        }
+      });
+      $('#sortable6').sortable({
+        cursor: 'move',
+        opacity: 0.5,
+        update: function(event, ui) {
+          var serial = $('#sortable6').sortable('serialize');
+          $.ajax({
+            url: "/task/sort",
+            type: "post",
+            data: serial
+          });
+        }
+      });
+      $('#sortable7').sortable({
+        cursor: 'move',
+        opacity: 0.5,
+        update: function(event, ui) {
+          var serial = $('#sortable7').sortable('serialize');
+          $.ajax({
+            url: "/task/sort",
+            type: "post",
+            data: serial
+          });
+        }
+      });
+      $('#sortable8').sortable({
+        cursor: 'move',
+        opacity: 0.5,
+        update: function(event, ui) {
+          var serial = $('#sortable8').sortable('serialize');
+          $.ajax({
+            url: "/task/sort",
+            type: "post",
+            data: serial
+          });
+        }
+      });
+      $('#sortable9').sortable({
+        cursor: 'move',
+        opacity: 0.5,
+        update: function(event, ui) {
+          var serial = $('#sortable9').sortable('serialize');
+          $.ajax({
+            url: "/task/sort",
+            type: "post",
+            data: serial
+          });
+        }
+      });
+      $('#sortable10').sortable({
+        cursor: 'move',
+        opacity: 0.5,
+        update: function(event, ui) {
+          var serial = $('#sortable10').sortable('serialize');
+          $.ajax({
+            url: "/task/sort",
+            type: "post",
+            data: serial
+          });
+        }
+      });
+      $('#sortable11').sortable({
+        cursor: 'move',
+        opacity: 0.5,
+        update: function(event, ui) {
+          var serial = $('#sortable11').sortable('serialize');
+          $.ajax({
+            url: "/task/sort",
+            type: "post",
+            data: serial
+          });
+        }
+      });
+      $('#sortable12').sortable({
+        cursor: 'move',
+        opacity: 0.5,
+        update: function(event, ui) {
+          var serial = $('#sortable12').sortable('serialize');
+          $.ajax({
+            url: "/task/sort",
+            type: "post",
+            data: serial
+          });
+        }
+      });
+      $('#sortable13').sortable({
+        cursor: 'move',
+        opacity: 0.5,
+        update: function(event, ui) {
+          var serial = $('#sortable13').sortable('serialize');
+          $.ajax({
+            url: "/task/sort",
+            type: "post",
+            data: serial
+          });
+        }
+      });
+      $('#sortable14').sortable({
+        cursor: 'move',
+        opacity: 0.5,
+        update: function(event, ui) {
+          var serial = $('#sortable14').sortable('serialize');
+          $.ajax({
+            url: "/task/sort",
+            type: "post",
+            data: serial
+          });
+        }
+      });
+      $('#sortable15').sortable({
+        cursor: 'move',
+        opacity: 0.5,
+        update: function(event, ui) {
+          var serial = $('#sortable15').sortable('serialize');
+          $.ajax({
+            url: "/task/sort",
+            type: "post",
+            data: serial
+          });
+        }
+      });
+      $('#sortable16').sortable({
+        cursor: 'move',
+        opacity: 0.5,
+        update: function(event, ui) {
+          var serial = $('#sortable16').sortable('serialize');
           $.ajax({
             url: "/book/sort",
             type: "post",
             data: serial
           });
-        };
+        }
       });
-    };
+      $('#sortable17').sortable({
+        cursor: 'move',
+        opacity: 0.5,
+        update: function(event, ui) {
+          var serial = $('#sortable17').sortable('serialize');
+          $.ajax({
+            url: "/book/sort",
+            type: "post",
+            data: serial
+          });
+        }
+      });
+      $('#sortable18').sortable({
+        cursor: 'move',
+        opacity: 0.5,
+        update: function(event, ui) {
+          var serial = $('#sortable18').sortable('serialize');
+          $.ajax({
+            url: "/book/sort",
+            type: "post",
+            data: serial
+          });
+        }
+      });
+      $('#sortable19').sortable({
+        cursor: 'move',
+        opacity: 0.5,
+        update: function(event, ui) {
+          var serial = $('#sortable19').sortable('serialize');
+          $.ajax({
+            url: "/book/sort",
+            type: "post",
+            data: serial
+          });
+        }
+      });
+      $('#sortable20').sortable({
+        cursor: 'move',
+        opacity: 0.5,
+        update: function(event, ui) {
+          var serial = $('#sortable20').sortable('serialize');
+          $.ajax({
+            url: "/book/sort",
+            type: "post",
+            data: serial
+          });
+        }
+      });
+      $('#sortable21').sortable({
+        cursor: 'move',
+        opacity: 0.5,
+        update: function(event, ui) {
+          var serial = $('#sortable21').sortable('serialize');
+          $.ajax({
+            url: "/book/sort",
+            type: "post",
+            data: serial
+          });
+        }
+      });
+      $('#sortable22').sortable({
+        cursor: 'move',
+        opacity: 0.5,
+        update: function(event, ui) {
+          var serial = $('#sortable22').sortable('serialize');
+          $.ajax({
+            url: "/book/sort",
+            type: "post",
+            data: serial
+          });
+        }
+      });
+      $('#sortable23').sortable({
+        cursor: 'move',
+        opacity: 0.5,
+        update: function(event, ui) {
+          var serial = $('#sortable23').sortable('serialize');
+          $.ajax({
+            url: "/book/sort",
+            type: "post",
+            data: serial
+          });
+        }
+      });
+      $('#sortable24').sortable({
+        cursor: 'move',
+        opacity: 0.5,
+        update: function(event, ui) {
+          var serial = $('#sortable24').sortable('serialize');
+          $.ajax({
+            url: "/book/sort",
+            type: "post",
+            data: serial
+          });
+        }
+      });
+      $('#sortable25').sortable({
+        cursor: 'move',
+        opacity: 0.5,
+        update: function(event, ui) {
+          var serial = $('#sortable25').sortable('serialize');
+          $.ajax({
+            url: "/book/sort",
+            type: "post",
+            data: serial
+          });
+        }
+      });
+      $('#sortable26').sortable({
+        cursor: 'move',
+        opacity: 0.5,
+        update: function(event, ui) {
+          var serial = $('#sortable26').sortable('serialize');
+          $.ajax({
+            url: "/book/sort",
+            type: "post",
+            data: serial
+          });
+        }
+      });
+      $('#sortable27').sortable({
+        cursor: 'move',
+        opacity: 0.5,
+        update: function(event, ui) {
+          var serial = $('#sortable27').sortable('serialize');
+          $.ajax({
+            url: "/book/sort",
+            type: "post",
+            data: serial
+          });
+        }
+      });
+      $('#sortable28').sortable({
+        cursor: 'move',
+        opacity: 0.5,
+        update: function(event, ui) {
+          var serial = $('#sortable28').sortable('serialize');
+          $.ajax({
+            url: "/book/sort",
+            type: "post",
+            data: serial
+          });
+        }
+      });
+      $('#sortable29').sortable({
+        cursor: 'move',
+        opacity: 0.5,
+        update: function(event, ui) {
+          var serial = $('#sortable29').sortable('serialize');
+          $.ajax({
+            url: "/book/sort",
+            type: "post",
+            data: serial
+          });
+        }
+      });
+      $('#sortable30').sortable({
+        cursor: 'move',
+        opacity: 0.5,
+        update: function(event, ui) {
+          var serial = $('#sortable30').sortable('serialize');
+          $.ajax({
+            url: "/book/sort",
+            type: "post",
+            data: serial
+          });
+        }
+      });
       for (  var i = 1;  i < 6;  i++  ) {
-        $('.sortable'+String(i)).bind('sortstop',function(){
+        $('#sortable'+String(i)).bind('sortstop',function(){
           // 番号を設定している要素に対しループ処理
             $(this).find('[name="num_data"]').each(function(idx){
           // タグ内に通し番号を設定（idxは0始まりなので+1する）
@@ -23,7 +369,7 @@ $(document).ready(function() {
           });
       };
       for (  var i = 6;  i < 11;  i++  ) {
-        $('.sortable'+String(i)).bind('sortstop',function(){
+        $('#sortable'+String(i)).bind('sortstop',function(){
           // 番号を設定している要素に対しループ処理
             $(this).find('[name="num_data2"]').each(function(idx){
           // タグ内に通し番号を設定（idxは0始まりなので+1する）
@@ -32,7 +378,7 @@ $(document).ready(function() {
           });
       };
       for (  var i = 11;  i < 16;  i++  ) {
-        $('.sortable'+String(i)).bind('sortstop',function(){
+        $('#sortable'+String(i)).bind('sortstop',function(){
           // 番号を設定している要素に対しループ処理
             $(this).find('[name="num_data3"]').each(function(idx){
           // タグ内に通し番号を設定（idxは0始まりなので+1する）
@@ -41,7 +387,7 @@ $(document).ready(function() {
           });
       };
       for (  var i = 16;  i < 21;  i++  ) {
-        $('.sortable'+String(i)).bind('sortstop',function(){
+        $('#sortable'+String(i)).bind('sortstop',function(){
           // 番号を設定している要素に対しループ処理
             $(this).find('[name="num_data"]').each(function(idx){
           // タグ内に通し番号を設定（idxは0始まりなので+1する）
@@ -50,7 +396,7 @@ $(document).ready(function() {
           });
       };
       for (  var i = 21;  i < 26;  i++  ) {
-        $('.sortable'+String(i)).bind('sortstop',function(){
+        $('#sortable'+String(i)).bind('sortstop',function(){
           // 番号を設定している要素に対しループ処理
             $(this).find('[name="num_data2"]').each(function(idx){
           // タグ内に通し番号を設定（idxは0始まりなので+1する）
@@ -59,7 +405,7 @@ $(document).ready(function() {
           });
       };
       for (  var i = 26;  i < 31;  i++  ) {
-        $('.sortable'+String(i)).bind('sortstop',function(){
+        $('#sortable'+String(i)).bind('sortstop',function(){
           // 番号を設定している要素に対しループ処理
             $(this).find('[name="num_data3"]').each(function(idx){
           // タグ内に通し番号を設定（idxは0始まりなので+1する）
@@ -69,7 +415,7 @@ $(document).ready(function() {
       };
 });
   for (  var i = 1;  i < 31;  i++  ) {
-    $('.sortable'+String(i)).disableSelection();
+    $('#sortable'+String(i)).disableSelection();
   }
 
 function done_alert() {
