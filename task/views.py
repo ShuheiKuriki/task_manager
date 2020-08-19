f256303b32f7b46e2b6bf777527adaf40c0a0aca
shuhei <shukuri.7336.8@gmail.com>
2020-08-19 09:22:18 +0900

    デザイン整備

--- task/views.py
+++ task/views.py
@@ -70,7 +70,7 @@ def list(request,pk):
         if info.num>0:
             tom_infos.append(info)
     other = Taskinfo(name="明日以降",tasks=tasks.filter(when__gt=datetime.date.today()+datetime.timedelta(days=1)).order_by('when'))
-    return render(request, 'task/list.html', {'today_infos':today_infos, 'today_num':today_num, 'tom_infos':tom_infos, 'tom_num':tom_num, 'other':other})
+    return render(request, 'task/task_list.html', {'today_infos':today_infos, 'today_num':today_num, 'tom_infos':tom_infos, 'tom_num':tom_num, 'other':other})
     
 def done_list(request,pk):
     if request.user.pk != pk:
