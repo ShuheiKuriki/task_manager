f5d4a53a758c64615f22c69baae59b36dd5dab1f
ShuheiKuriki <shukuri.7336.8@gmail.com>
2023-01-15 18:12:37 +0900

    DBをSQLiteに変更（herokuでpostgreの使用をやめるため）

--- taskManager/settings/prod.py
+++ taskManager/settings/prod.py
@@ -44,9 +44,15 @@ LOGGING = {
     },
 }
 
-import dj_database_url
+# import dj_database_url
+# DATABASES = {
+#     'default': dj_database_url.config()
+# }
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+    }
 }
 
 SECRET_KEY = os.environ['SECRET_KEY']
